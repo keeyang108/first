96eff6846c7b07b12df541a090f69e012fa268e9
kee <keeyang108@163.com>
2016-11-15 17:50:52 +0800

    add loginInterceptor and perfect logback.xml

---
+++ first/src/main/java/com/extra/first/aop/RightAspect.java
@@ -0,0 +1,17 @@
+package com.extra.first.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Kee on 2016/11/7.
+ */
+@Aspect
+@Component
+public class RightAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(RightAspect.class);
+
+}
---
+++ first/src/main/java/com/extra/first/interceptor/LoginInterceptor.java
@@ -0,0 +1,42 @@
+package com.extra.first.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 登录拦截器
+ * Created by Administrator on 2016/11/15.
+ */
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+    private Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
+
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        String requestUri = httpServletRequest.getRequestURI();
+        String contextPath = httpServletRequest.getContextPath();
+        String url = requestUri.substring(contextPath.length());
+
+        logger.info("requestUri:{}",requestUri);
+        logger.info("contextPath:{}",contextPath);
+        logger.info("url:{}",url);
+
+        String userName = httpServletRequest.getSession().getAttribute("supervisor").toString();
+        if (userName == null){
+            httpServletRequest.getRequestDispatcher("/WEB-INF/sign-in.html").forward(httpServletRequest,httpServletResponse);
+            return false;
+        }
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
